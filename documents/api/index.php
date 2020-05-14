b642a546d8bf84023a340ce9bf88e332a40cf5d2
Michael Wagner <mwagn4@gmail.com>
2020-05-14 22:33:45 +0200

    Adds icon, webmanifest and service worker for PWA

--- documents/api/index.php
+++ documents/api/index.php
@@ -30,10 +30,13 @@ $router->add('DELETE', '/api/files/[a-z0-9-]+', new Http\Handler\DeleteFile($rep
 $router->add('POST', '/api/files/[a-z0-9-]+/tags', new Http\Handler\AddTag($repository));
 $router->add('GET',  '/api/files/[a-z0-9-]+/raw', new Http\Handler\RawFile($repository));
 
-$router->add('POST', '/api/scan', new Http\Handler\ScanImage($scanner, $repository));
+$router->add('POST', '/api/scan', new Http\Handler\ScanImage($scanner));
 
 foreach(array(
+  '/icon.png',
   '/index.html',
+  '/service-worker.js',
+  '/manifest.webmanifest',
 
   '/app/async.js',
   '/app/dom.js',
