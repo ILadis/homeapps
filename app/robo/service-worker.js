cde20a2cbf0e602007c33301accdd80cccd538b1
Michael Wagner <mwagn4@gmail.com>
2020-10-09 22:08:16 +0200

    Adds service worker for robo app

---
+++ app/robo/service-worker.js
@@ -0,0 +1,45 @@
+
+self.oninstall = (event) => event.waitUntil(refreshAssets());
+async function refreshAssets() {
+  let cache = await caches.open('app');
+  let resources = [
+    '/',
+    '/icon.png',
+    '/index.html',
+    '/service-worker.js',
+    '/manifest.webmanifest',
+
+    '/app/dom.js',
+    '/app/presenter.js',
+    '/app/client.js',
+    '/app/views.js',
+
+    '/app/styles.css',
+  ];
+
+  for (let url of resources) {
+    let request = new Request(url);
+    let response = await fetch(request);
+    await cache.put(request, response);
+  }
+}
+
+self.onactivate = (event) => clients.claim();
+
+self.onfetch = (event) => {
+  if (event.request.method == 'GET') {
+    event.respondWith(handleRequest(event));
+  }
+};
+
+async function handleRequest({ request }) {
+  let cache = await caches.open('app');
+  let response = await cache.match(request);
+
+  if (!response) {
+    response = await fetch(request);
+  }
+
+  return response;
+}
+
