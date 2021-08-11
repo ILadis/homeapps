b57f3f9630e965600b7705d09d695fb05e7c4121
Michael Wagner <mwagn4@gmail.com>
2021-08-11 23:33:26 +0200

    Formats server version and uptime

--- app/ts3stats/presenter.js
+++ app/ts3stats/presenter.js
@@ -14,9 +14,9 @@ Presenter.prototype.showIndex = async function() {
   async function refreshInfo() {
     let info = await client.serverInfo();
     // TODO format version and uptime
-    serverInfo.add('Servername', info.name);
-    serverInfo.add('Version', info.version);
-    serverInfo.add('Laufzeit', info.uptime);
+    serverInfo.add('Name', info.name);
+    serverInfo.add('Version', formatVersion(info));
+    serverInfo.add('Laufzeit', formatUptime(info));
   }
 
   async function refreshClients() {
@@ -24,6 +24,29 @@ Presenter.prototype.showIndex = async function() {
     for (let client of clients) {
       clientList.add(client);
     }
+  }
+
+  function formatVersion({ version }) {
+    let pattern = RegExp('(\\d+\\.)+\\d');
+    let matches = pattern.exec(version);
+    return matches ? matches[0] : '';
+  }
+
+  function formatUptime({ uptime }) {
+    let steps = new Set([
+      ['Tag(e)', 60*60*24],
+      ['Stunde(n)', 60*60],
+    ]);
+
+    let format = '', value;
+
+    for (let [unit, seconds] of steps) {
+      value = Math.floor(uptime / seconds);
+      uptime %= seconds;
+      format += value + ' ' + unit + ' ';
+    }
+
+    return format;
   }
 };
 
